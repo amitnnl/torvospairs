b572f5b0e4353e8598d5499d3001a11438e00def
Aamya Holidays <deploy@aamyaholidays.com>
2026-04-12 11:49:51 +0530

    Expand CSS debug script

--- portal/debug_css.php
+++ portal/debug_css.php
@@ -1,5 +1,19 @@
 <?php
-$file = __DIR__ . '/assets/css/portal.css';
-echo "File exists: " . (file_exists($file) ? 'YES' : 'NO') . "<br>";
-echo "Path: " . $file;
+echo "<h3>Contents of portal/</h3>";
+$dir = __DIR__;
+$files = scandir($dir);
+foreach ($files as $file) {
+    echo $file . (is_dir($dir . '/' . $file) ? ' [DIR]' : '') . "<br>";
+}
+
+echo "<h3>Check portal/assets/</h3>";
+$assetsDir = $dir . '/assets';
+if (file_exists($assetsDir)) {
+    $files = scandir($assetsDir);
+    foreach ($files as $file) {
+        echo $file . (is_dir($assetsDir . '/' . $file) ? ' [DIR]' : '') . "<br>";
+    }
+} else {
+    echo "assets folder does NOT exist";
+}
 ?>
